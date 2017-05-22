12769bbe6df5e71a8ce785ff1905a5a8c0545c96
DudiZagga <dudizagga@gmail.com>
2017-05-22 18:28:58 +0300

    updates

--- app/src/main/java/com/example/moree/mytvapp1/Fragmentcontainer.java
+++ app/src/main/java/com/example/moree/mytvapp1/Fragmentcontainer.java
@@ -3,10 +3,18 @@ package com.example.moree.mytvapp1;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.baoyz.widget.PullRefreshLayout;
 
@@ -14,6 +22,8 @@ public class Fragmentcontainer extends AppCompatActivity {
     FragmentManager fm;
     FragmentTransaction ft;
     LinearLayout conteiner;
+    ImageButton wifi;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +32,18 @@ public class Fragmentcontainer extends AppCompatActivity {
     }
 
     private void setPointer() {
+        this.context=this;
         conteiner=(LinearLayout)findViewById(R.id.fcontainer);
+        wifi=(ImageButton)findViewById(R.id.btnwifi);
         fm = getFragmentManager();
         ft = fm.beginTransaction();
         ft.add(R.id.fcontainer, new Panel());
         ft.commit();
+        wifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));            }
+        });
     }
     public void nextFragment(int container, Fragment className) {
         fm=getFragmentManager();
--- app/src/main/java/com/example/moree/mytvapp1/MainActivity.java
+++ app/src/main/java/com/example/moree/mytvapp1/MainActivity.java
@@ -4,32 +4,37 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentContainer;
+import android.provider.Settings;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
-import com.backendless.async.callback.BackendlessCallback;
 import com.backendless.exceptions.BackendlessException;
 import com.backendless.exceptions.BackendlessFault;
-import com.backendless.persistence.BackendlessDataQuery;
-import com.backendless.property.UserProperty;
 
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import weborb.client.Fault;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
     EditText userId, usePass;
     Button Login, register;
     Fragmentcontainer fragmentcontainer;
-
+    TextInputLayout input_Email, input_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +51,9 @@ public class MainActivity extends AppCompatActivity {
         usePass = (EditText) findViewById(R.id.usePass);
         Login = (Button) findViewById(R.id.btnLogin);
         register = (Button) findViewById(R.id.InRegister);
+        input_Email = (TextInputLayout) findViewById(R.id.Input_Email);
+        input_pass = (TextInputLayout) findViewById(R.id.Input_pass);
         fragmentcontainer = new Fragmentcontainer();
-
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,72 +66,60 @@ public class MainActivity extends AppCompatActivity {
                 Register();
             }
         });
-
     }
+
+
 
     private void Login() {
-        final String Myuser = userId.getText().toString();
-        final String MyPass = usePass.getText().toString();
+        String Myuser = userId.getText().toString();
+        String Mypass = usePass.getText().toString();
 
-        /*Intent First = new Intent(context, Fragmentcontainer.class);
-        startActivity(First);*/
-
-
-//        try
-//        {
-//            BackendlessUser user=Backendless.UserService.login(Myuser,MyPass);
-//        }
-//        catch( BackendlessException exception )
-//        {
-//            // login failed, to get the error code, call exception.getFault().getCode()
-//            Toast.makeText(context, "Exception: "+exception, Toast.LENGTH_SHORT).show();
-//        }
+        if (userId.getText().toString().length() < 1 && usePass.getText().toString().length() < 1) {
+            input_Email.setErrorTextAppearance(R.style.error_appearance);
+            input_Email.setError("Enter you Email");
+            return;
+        }
+        try {
 
 
+            Backendless.UserService.login(Myuser, Mypass, new AsyncCallback<BackendlessUser>() {
+                @Override
+                public void handleResponse(BackendlessUser response) {
+                    Intent First = new Intent(context, Fragmentcontainer.class);
+                    startActivity(First);
+                }
 
-        Backendless.UserService.login(Myuser , MyPass, new AsyncCallback<BackendlessUser>() {
-            @Override
-            public void handleResponse(BackendlessUser response) {
-                Toast.makeText(context, "Logged"+Myuser, Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(context,Fragmentcontainer.class));
-            }
+                @Override
+                public void handleFault(BackendlessFault fault) {
 
-            @Override
-            public void handleFault(BackendlessFault fault) {
-                Toast.makeText(context, "Error: " + fault.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-/*
-        if (userId.getText().toString().length()<1 && usePass.getText().toString().length()<1)
-        {
-            Toast.makeText(context, "Please enter Email or Password", Toast.LENGTH_SHORT).show();
-        return;
+
+                    if (fault.getCode().equals("3003")) {
+                        input_Email.setErrorTextAppearance(R.style.error_appearance);
+                        input_Email.setError("Email Not registed");
+                    } else {
+                        Toast.makeText(context, "Network Error ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
-        Toast.makeText(context, "my id" + userId.getText().toString() + usePass.getText().toString(), Toast.LENGTH_SHORT).show();
-        Backendless.UserService.login(Myuser, Mypass, new AsyncCallback<BackendlessUser>() {
-            @Override
-            public void handleResponse(BackendlessUser response) {
-
-            }
-
-            @Override
-            public void handleFault(BackendlessFault fault) {
-                Toast.makeText(context, "Error" + fault.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-*/
     }
+
 
     private void Register() {
         final AlertDialog myRegister = new AlertDialog.Builder(context).create();
         //final AlertDialog optionAlert =myRegister.create();
         View inflateRegister = LayoutInflater.from(context).inflate(R.layout.register, null, false);
         myRegister.setCanceledOnTouchOutside(false);
+        final TextInputLayout Email = (TextInputLayout) inflateRegister.findViewById(R.id.Email);
+        final TextInputLayout pass1 = (TextInputLayout) inflateRegister.findViewById(R.id.layout_Pass1);
+        final TextInputLayout pass2 = (TextInputLayout) inflateRegister.findViewById(R.id.layout_Pass2);
         final EditText username = (EditText) inflateRegister.findViewById(R.id.RegisterName);
         final EditText userpass1 = (EditText) inflateRegister.findViewById(R.id.userPass1);
         final EditText userpass2 = (EditText) inflateRegister.findViewById(R.id.userPass2);
+        //userpass1.setTransformationMethod(new PasswordTransformationMethod());
         Button btnRegister = (Button) inflateRegister.findViewById(R.id.btnRegister);
         Button btnCancel = (Button) inflateRegister.findViewById(R.id.btnCancel);
         btnCancel.setOnClickListener(new View.OnClickListener() {
@@ -138,16 +132,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (username.getText().toString().isEmpty() || username.getText().toString().length() < 4) {
-                    username.setError("Enter your E-mail please");
-                } else if (userpass1.getText().toString().length() < 6 || userpass1.getText().toString().isEmpty()) {
-                    userpass1.setError("Password needs 6 characters and more");
-                } else if (!userpass1.getText().toString().equals(userpass2.getText().toString())) {
-                    userpass2.setError("Password dont match");
-                } else {
+                    Email.setErrorTextAppearance(R.style.error_appearance);
+                    Email.setError("Enter Email");
 
-                    BackendlessUser RegisterUser = new BackendlessUser();
-                    RegisterUser.setEmail(username.getText().toString());
-                    RegisterUser.setPassword(userpass1.getText().toString());
+                }
+
+                if (userpass1.getText().toString().length() < 6 || userpass1.getText().toString().isEmpty()) {
+                    pass1.setError("Not a valid Password");
+                    pass1.setErrorTextAppearance(R.style.error_appearance);
+
+                }
+
+                if (!userpass1.getText().toString().equals(userpass2.getText().toString())) {
+                    pass2.setError("Password dont match");
+                    pass2.setErrorTextAppearance(R.style.error_appearance);
+
+                }
+
+                BackendlessUser RegisterUser = new BackendlessUser();
+                RegisterUser.setProperty("email", username.getText().toString());
+                RegisterUser.setProperty("password", userpass1.getText().toString());
+                try {
+
+
                     Backendless.UserService.register(RegisterUser, new AsyncCallback<BackendlessUser>() {
                         @Override
                         public void handleResponse(BackendlessUser response) {
@@ -158,10 +165,19 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void handleFault(BackendlessFault fault) {
-                            Toast.makeText(context, "Error" + fault.getMessage(), Toast.LENGTH_SHORT).show();
-
+                            if (fault.getCode().equals("3033")) {
+                                Email.setErrorTextAppearance(R.style.error_appearance);
+                                Email.setError("Email Already Exists");
+                            }
+                            if (fault.getCode().equals("3040")) {
+                                Email.setErrorTextAppearance(R.style.error_appearance);
+                                Email.setError("Not Valid Email");
+                            }
                         }
                     });
+
+                } catch (Exception e) {
+                    Toast.makeText(context, "Please Enter the Following!", Toast.LENGTH_SHORT).show();
                 }
             }
 
